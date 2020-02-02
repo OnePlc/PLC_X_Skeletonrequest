d3a62b108afc888b9bdc92e9a18c72c0bbcbe802
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-02-02 05:00:38 +0100

    sql fixes

---
+++ data/data_simple.sql
@@ -0,0 +1,5 @@
+--
+-- Matching Partial
+--
+INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
+(NULL, 'partial', 'Matching', 'matching', 'skeletonrequest-matching', 'skeletonrequest-single', 'col-md-12', '', '', 0, 1, 0, '', '', '');
